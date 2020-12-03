82d1b302daf526a7a41f4bfc01cb8f6c4a8fbd33
Juanma <jmlpdr.93@gmail.com>
2020-12-03 11:47:23 +0000

    first commit

---
+++ src/main/java/com/juanma/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.juanma.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+	// Un cambio Otro cambio
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
